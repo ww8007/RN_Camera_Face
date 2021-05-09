c1ec2a80f46ff5efdf25f879dac4d8fd549591ad
ww8007 <wshmin@naver.com>
2021-05-09 23:35:50 +0900

    카메라에 대한 아주 간단한 ui 작성

--- Camera/App.js
+++ Camera/App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [set, onSet] = useState(true);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              onSet(!set);
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+          {set ? (
+            <View style={styles.TextContainer}>
+              <Text style={styles.subText}>온도는 37도 입니다.</Text>
+              <Text style={styles.subText}>정상입니다.</Text>
+            </View>
+          ) : null}
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -14,8 +47,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+  },
+  TextContainer: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "green",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  subText: {
+    textAlign: "center",
+    fontSize: 32,
+    color: "white",
+  },
+  button: {
+    flex: 5,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
   },
 });
---
+++ Camera/Camera.js
@@ -0,0 +1,4 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Camera } from "expo-camera";
+import { StyleSheet, Text, View } from "react-native";
